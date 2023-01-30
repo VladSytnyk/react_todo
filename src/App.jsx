eb6e974b07f460cec45e8c5f4c962cc6b6b1d5c7
VladSytnyk <vladsytnyk95@gmail.com>
2023-01-30 16:13:54 +0100

    added empty arr to state

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ function App() {
   const todoNameRef = useRef();
 
   useEffect(() => {
-    const storeTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    const storeTodos =
+      JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
     if (storeTodos) setTodos(storeTodos);
   }, []);
 
